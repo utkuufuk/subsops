7dc040eb117bdeb7340999d7af698fe7168ad699
utkuufuk <utkuufuk@gmail.com>
2019-05-12 19:13:34 +0300

    fix a bug about sending post nofification emails with HTML content

--- README.md
+++ README.md
@@ -1,14 +1,47 @@
 # subops
 Manages subscriber operations for [my blog](https://utkuufuk.github.io).
 
-## Subscription
+## Serve Functions Locally or Deploy to Cloud
+Serve functions locally without deploying functions to cloud:
+``` sh
+# default port is 5000
+sudo firebase serve --only functions --port=5123
+```
+
+Deploy functions to cloud:
+``` sh
+firebase deploy --only functions
+```
+
+## Configuration
+### Set Remote Configuration
+```sh
+# set admin username & password
+firebase functions:config:set admin.email="utkuufuk" admin.password="ilikebananas"
+
+# set trello secret
+firebase functions:config:set blog.website="https://utkuufuk.github.io"
+```
+
+### Retrieve Current Envvironment Configuration
+```sh
+firebase functions:config:get
+```
+
+### Import Remote Configuration to Local Environment
+```sh
+firebase functions:config:get > functions/.runtimeconfig.json
+```
+
+## Sequence Diagrams
+### Subscription
 ![Subscription](plantuml/subscribe.png)
 
-## Confirmation
+### Confirmation
 ![Confirmation](plantuml/confirm.png)
 
-## Blog Post Notification
+### Blog Post Notification
 ![Publish](plantuml/publish.png)
 
-## Unsubscription
+### Unsubscription
 ![Unsubscription](plantuml/unsubscribe.png)
--- functions/index.js
+++ functions/index.js
@@ -1,39 +1,102 @@
-'use strict';
+"use strict";
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const nodemailer = require('nodemailer');
-const validator = require('email-validator');
-const cors = require('cors')({origin: true});
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const nodemailer = require("nodemailer");
+const validator = require("email-validator");
+const cors = require("cors")({origin: true});
 const settings = {timestampsInSnapshots: true};
 admin.initializeApp(functions.config().firebase);
 var db = admin.firestore();
 db.settings(settings);
 
 // sends an email using admin creds
-function sendEmail(address, header, body) {
+function sendEmail(address, header, body, contentType) {
     const mailTransport = nodemailer.createTransport({
-        service: 'gmail',
-        host: 'smtp.gmail.com',
+        service: "gmail",
+        host: "smtp.gmail.com",
         auth: {
             user: functions.config().admin.email,
             pass: functions.config().admin.password
         }
     });
-    return mailTransport.sendMail({
+
+    const options = {
         from: `"Utku Ufuk" <${functions.config().admin.email}>`,
         to: address,
         subject: header,
-        text: body
-    });
+        [contentType]: body
+    };
+
+    return mailTransport.sendMail(options);
 }
 
-// updates subscriber state
+function insertSubscriber(response, sub) {
+    sub.state = "unconfirmed";
+    sub.createDate = new Date();
+    sub.updateDate = new Date();
+    return db
+        .collection("subscribers")
+        .add(sub)
+        .then(result => {
+            // send a confirmation email to the new subscriber
+            sub.id = result.id;
+            let body =
+                `Hey ${sub.name},\n\nThank you for subscribing to my blog.\n\n` +
+                `Please click the link below to confirm your subscription:\n\n` +
+                `${functions.config().blog.website}/complete/?name=${sub.name}` +
+                `&id=${sub.id}\n\nCheers,\n\nUtku`;
+            const confirmPromise = sendEmail(sub.email, "Welcome to Utku's Blog!", body, "text");
+
+            // send a new subscriber notificaiton email
+            let subject = "You Have a New Unconfirmed Subsciber!";
+            body = "ID: " + sub.id + "\nName: " + sub.name + "\nEmail: " + sub.email;
+            const notifyPromise = sendEmail(functions.config().admin.email, subject, body, "text");
+            return Promise.all([confirmPromise, notifyPromise]);
+        })
+        .then(results => {
+            console.log(
+                `Confirmation email sent: ${results[0]}` +
+                    `\nNew subscriber notification sent: ${sub.id}, ${sub.name}, ${sub.email}`
+            );
+            return response.status(200).send(sub.name + ":" + sub.id);
+        })
+        .catch(err => {
+            console.error(err);
+            return response.status(500).send("An unexpected server error occurred.");
+        });
+}
+
+function createSubscriber(request, response) {
+    const sub = request.body;
+    const valid =
+        sub.name !== undefined &&
+        sub.name.length > 0 &&
+        sub.name.length < 20 &&
+        validator.validate(sub.email) &&
+        new RegExp(/^([a-zA-ZöçşığüÖÇŞİĞÜ]+)$/).test(sub.name);
+    if (!valid) {
+        const body =
+            `Illegal subscriber creation attempt:\nEvent Date: ` +
+            `${new Date().toISOString()} \nName: ${sub.name}\nEmail ${sub.email}`;
+        return sendEmail(functions.config().admin.email, "Illegal Subscription", body, "text")
+            .then(result => {
+                console.warn("Warning mail sent:", result, "\n", body);
+                return response.status(400).send(body);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
+    return insertSubscriber(response, sub);
+}
+
 function updateSubscriber(request, response, type, oldState, newState) {
-    return cors(request, response, () => {
-        const id = request.body.id;
-        let subject, message, valid;
-        return db.doc('subscribers/' + id).get()
+    const id = request.body.id;
+    let subject, message, valid;
+    return db
+        .doc("subscribers/" + id)
+        .get()
         .then(result => {
             const sub = result.data();
             const promises = [];
@@ -48,7 +111,8 @@ function updateSubscriber(request, response, type, oldState, newState) {
                 sub["updateDate"] = new Date();
                 promises.push(db.doc("subscribers/" + id).set(sub));
             }
-            promises.unshift(sendEmail(functions.config().admin.email, subject, message));
+            const mailPromise = sendEmail(functions.config().admin.email, subject, message, "text");
+            promises.unshift(mailPromise);
             return Promise.all(promises);
         })
         .then(results => {
@@ -57,99 +121,58 @@ function updateSubscriber(request, response, type, oldState, newState) {
         })
         .catch(err => {
             console.error(err);
-            return response.status(500).send('An unexpected server error occurred.');
+            return response.status(500).send("An unexpected server error occurred.");
         });
-    });
 }
 
 // notifies confirmed subscribers upon a blogpost creation event on firestore
-exports.notifySubscribers = functions.firestore.document('blogposts/{id}').onCreate((snap, _) => {
+exports.publish = functions.firestore.document("blogposts/{id}").onCreate((snap, _) => {
     const post = snap.data();
-    db.collection("subscribers").where('state', '==', 'confirmed').get()
-    .then(result => {
-        const promises = [];
-        result.forEach(result => {
-            // send an email to each confirmed subscriber
-            const sub = result.data();
-            const body = 
-                `Hey ${sub.name},<p>Check out my new blog post:<br>${post.url}<p>Have a nice day,` +
-                `<p>Utku<p><a style="font-size: 9px" href=${functions.config().blog.website}` +
-                `/unsubscribe/?name=${sub.name}&id=${sub.id}">unsubscribe</a>`;
-            promises.push(sendEmail(sub.email, post.header, body));
-            console.log(
-                `New blog post notification email sent:` +
-                `${sub.name}, ${sub.email}, ${post.header}, ${post.url}`);
+    return db
+        .collection("subscribers")
+        .where("state", "==", "confirmed")
+        .get()
+        .then(result => {
+            const promises = [];
+            result.forEach(result => {
+                const sub = result.data();
+                const body =
+                    `Hey ${sub.name},<p>Check out my new blog post:<br>${post.url}` +
+                    `<p>Have a nice day,<p>Utku<p><a style="font-size: 9px" href=${
+                        functions.config().blog.website
+                    }/unsubscribe/?name=${sub.name}&id=${result.id}>unsubscribe</a>`;
+                const mailPromise = sendEmail(sub.email, post.header, body, "html");
+                promises.push(mailPromise);
+                console.log(
+                    `Blog post notification email sent: `,
+                    `${sub.name}, ${sub.email}, ${post.header}, ${post.url}`
+                );
+            });
+            console.log("All blog post notification emails sent.");
+            return Promise.all(promises);
+        })
+        .catch(err => {
+            console.error(err);
         });
-        console.log("All blog post notification emails sent.");
-        return Promise.all(promises);
-    })
-    .catch(err => {
-        console.error(err);
-    });
 });
 
 // creates a new subscriber
 exports.subscribe = functions.https.onRequest((request, response) => {
     return cors(request, response, () => {
-        // check if subscriber creation attempt is legal
-        let sub = request.body;
-        let valid = 
-            sub.name !== undefined && sub.name.length > 0 && sub.name.length < 20 &&
-            validator.validate(sub.email) && new RegExp(/^([a-zA-ZöçşığüÖÇŞİĞÜ]+)$/).test(sub.name);
-        if (!valid) {
-            // send a warning email and reject the request
-            const body = 
-                `Illegal subscriber creation attempt:\nEvent Date: ` +
-                `${new Date().toISOString()} \nName: ${sub.name}\nEmail ${sub.email}`;
-            return sendEmail(functions.config().admin.email, "Illegal Subscriber Creation", body)
-            .then(result => {
-                console.warn("Warning mail sent:", result, "\n", body);
-                return response.status(400).send(body);
-            })
-            .catch(err => {
-                console.error(err);
-            })
-        }
-        // accept the request & create a new subscriber in firestore
-        sub.state = "unconfirmed";
-        sub.createDate = new Date();
-        sub.updateDate = new Date();
-        return db.collection("subscribers").add(sub)
-        .then(result => {
-            // send a confirmation email to the new subscriber
-            sub.id = result.id;
-            let body =
-                `Hey ${sub.name},\n\nThank you for subscribing to my blog.\n\n` +
-                `Please click the link below to confirm your subscription:\n\n` +
-                `${functions.config().blog.website}/complete/?name=${sub.name}` +
-                `&id=${sub.id}\n\nCheers,\n\nUtku`;
-            const confirmPromise = sendEmail(sub.email, "Welcome to Utku's Blog!", body);
-
-            // send a new subscriber notificaiton email
-            let subject = "You Have a New Unconfirmed Subsciber!";
-            body = "ID: " + sub.id + "\nName: " + sub.name + "\nEmail: " + sub.email;
-            const notifyPromise = sendEmail(functions.config().admin.email, subject, body);
-            return Promise.all([confirmPromise, notifyPromise]);
-        })
-        .then(results => {
-            console.log(
-                `Confirmation email sent: ${results[0]}\nNew subscriber notification sent: ` + 
-                `${sub.id}, ${sub.name}, ${sub.email}`);
-            return response.status(200).send(sub.name + ":" + sub.id);
-        })
-        .catch(err => {
-            console.error(err);
-            return response.status(500).send('An unexpected server error occurred.');
-        });
+        return createSubscriber(request, response);
     });
 });
 
 // confirms a subscription
 exports.confirm = functions.https.onRequest((request, response) => {
-    return updateSubscriber(request, response, "Confirmation", "unconfirmed", "confirmed");
+    return cors(request, response, () => {
+        return updateSubscriber(request, response, "Confirmation", "unconfirmed", "confirmed");
+    });
 });
 
 // cancels a subscription
 exports.unsubscribe = functions.https.onRequest((request, response) => {
-    return updateSubscriber(request, response, "Unsubscription", "confirmed", "unsubscribed");
+    return cors(request, response, () => {
+        return updateSubscriber(request, response, "Unsubscription", "confirmed", "unsubscribed");
+    });
 });
